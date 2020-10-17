1d6ea179177a5a53dd98d31ae9a7e548223dab1c
zl645420646 <1804094055@qq.com>
2020-10-17 17:14:19 +0800

    1

---
+++ flutterShop/lib/route.dart
@@ -0,0 +1,9 @@
+import 'dart:js';
+
+import 'tabs.dart';
+import 'class/home/home.dart';
+import 'class/category/category.dart';
+import 'class/cart/cart.dart';
+import 'class/mine/mine.dart';
+
+final routes = {'/': (context) => TabBarPage()};
